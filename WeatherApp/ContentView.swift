51a8f43b0ce88b1611c6c5ac5c5cdd2b6ee6278f
lashu333 <140723330+lashu333@users.noreply.github.com>
2023-12-02 01:03:21 +0400

    weather app ui
    ui prototype? first try idk i did my best

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,126 @@
+// ContentView.swift
+// WeatherApp
+// Created by Lasha Tavberidze on 19.11.23.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            ukanafoni(topColor: .mint, botColor: .blue)
+            VStack {
+            CityView(CityName: "Georgia, Tbilisi")
+
+                VStack {
+                    mainWeatherView(temperature: 19, weatherImg: "moon.fill")
+                }
+                .padding(.bottom, 60)
+
+                HStack(spacing: 13) {
+                    WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.moon.rain.fill", temperature: 6)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "cloud.bolt.fill", temperature: 8)
+                    WeatherDayView(dayOfWeek: "THU", imageName: "cloud.sun.fill", temperature: 15)
+                    WeatherDayView(dayOfWeek: "FRI", imageName: "cloud.sun.bolt.fill", temperature: 11)
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.moon.fill", temperature: 10)
+                }
+
+                Spacer()
+
+                Button(action: {
+                    // Replace with the actual action you want to perform
+                    print("Button tapped")
+                }) {
+                    Text("Flip the day")
+                        .foregroundColor(.teal)
+                        .font(.system(size: 26, weight: .semibold, design: .serif))
+                        .frame(width: 200, height: 60)
+                        .background(.white)
+                        .cornerRadius(12)
+                }
+                .padding(.bottom, 70)
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                .foregroundColor(.yellow)
+                .shadow(color: .pink, radius: 15)
+
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .shadow(color: .blusha, radius: 10)
+                .frame(width: 60, height: 60, alignment: .center)
+
+            Text("\(temperature)°")
+                .font(.system(size: 27, weight: .semibold, design: .rounded))
+                .foregroundColor(.indigo)
+                .shadow(color: .blusha, radius: 10)
+        }
+    }
+}
+
+struct ukanafoni: View {
+    
+    var topColor: Color
+    var botColor : Color
+    
+    var body: some View {
+        
+        LinearGradient(gradient: Gradient(colors: [topColor, .blusha, botColor]),
+            startPoint: .top,
+            endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+
+struct CityView: View {
+    
+    var CityName: String
+    var body: some View {
+        
+        Text(CityName)
+            .font(.system(size: 32, weight: .light, design: .default))
+            .foregroundColor(.white)
+            .shadow(color: .accentColor, radius: 10)
+            .padding()
+        
+    }
+}
+struct mainWeatherView : View {
+    
+    var temperature : Int
+    var weatherImg : String
+    var body: some View {
+        Image(systemName: weatherImg)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .shadow(color: .accentColor, radius: 2)
+            .frame(width: 178, height: 178, alignment: .center)
+
+        Text("\(temperature)°")
+            .font(.system(size: 80, weight: .semibold, design: .rounded))
+            .foregroundColor(.indigo)
+            .shadow(color: .cyan, radius: 3)
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Lasha Tavberidze on 19.11.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
